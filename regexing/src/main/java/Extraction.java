9caf45f94ed245a7dfad0a109ff310216c3169df
GreedOrDie <greedordie@gmail.com>
2017-01-05 08:10:07 -0800

    Incomplete

--- regexing/src/main/java/Extraction.java
+++ regexing/src/main/java/Extraction.java
@@ -5,7 +5,7 @@ import java.util.List;
  * Created by jeff on 1/4/17.
  */
 public class Extraction {
-
+//\(?(\d{3})\)?[\-s\.]?(\d{3})[\-\s\.]?(\d{4})
     /**
      * Take an input string and extract all phone numbers, add them to the list, and return the list
      * @param input
@@ -13,6 +13,7 @@ public class Extraction {
      */
     public List<String> extractPhoneNumbers(String input){
         List<String> phones = new ArrayList<String>();
+
 
         return phones;
     }
--- regexing/src/main/java/Validation.java
+++ regexing/src/main/java/Validation.java
@@ -1,25 +1,52 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by jeff on 1/4/17.
  */
 public class Validation {
-
     /**
      * Test if the string is binary (Only 0 or 1)
      * @param input
      * @return
      */
-    public boolean binary(String input){
-        return true;
-    }
+    public boolean binary(String input) {
+        Validation v = new Validation();
+        Pattern p = Pattern.compile("[01]+");
+        Matcher m = p.matcher(input);
+        if (m.matches()) {
+            m.find();
+            if (m.find() == m.matches()) {}
+                return true;
+
+            } else {
+
+
+                return m.matches();
+            }
+        }
+
 
     /**
      * Test whether input is binary and even
      * @param input
      * @return
      */
-    public boolean binary_even(String input){
-        return true;
+    public boolean binary_even(String input) {
+        Validation v = new Validation();
+        Pattern p = Pattern.compile("0$");
+        Matcher m = p.matcher(input);
+        if (m.matches()) {
+            m.find();
+            if (m.find() == m.matches()) {
+                return true;
+            } else {
+                return m.matches();
+            }
+        }
+    return m.matches();
     }
+
 
     /**
      * Test if the input is a hexadecimal (0 - 9 or A, B, C, D, E, F)
